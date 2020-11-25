4a10272f3b5670b0ed1634a57e3ec8ad1a95517b
L-cuitain <948527099@qq.com>
2020-11-25 08:25:38 +0800

    DAY02

--- day01/01-HelloWorld/main.go
+++ day01/01-HelloWorld/main.go
@@ -1,7 +1,11 @@
 package main
 
+// 导入 fmt 包
 import "fmt"
 
+//函数外只能放置标识符 (变量/常量/函数/类型) 的声明
+
+// 程序入口函数
 func main() {
 	fmt.Println("HelloWorld")
 }
---
+++ day01/07-string/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//字符串
+
+func main() {
+	path := "'D:\\Go\\src\\studygo\\day01'"
+	fmt.Println(path)
+
+	s := "I'm ok"
+	fmt.Println(s)
+}
