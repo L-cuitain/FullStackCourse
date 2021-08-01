1a61d58f93c5a1861a90f8baadc67f2df724ffc2
L-cuitain <948527099@qq.com>
2021-08-01 17:05:49 +0800

    update: 20210413

--- day02-接口&类&函数/ts_function/function.ts
+++ day02-接口&类&函数/ts_function/function.ts
@@ -1,4 +1,5 @@
 //函数
+
 //返回值为字符串
 function test():string {
     return "调用函数"
@@ -45,3 +46,33 @@ var foo = (x:number) =>  10 + x;
 //函数重载
 // function add(x:string,y:string):string;
 // function add(x:number, y:number):number;
+
+//函数可选参数 ?标识符
+let fn = function(x:number , y?:number){
+    if(y){
+        return x + y;
+    }else{
+        return x;
+    }
+}
+
+fn(12);
+
+
+//参数默认值
+let fn1 = function(x:number , y:number = 3){
+    return x + y;
+}
+
+fn1(12);
+
+
+//rest参数(剩余参数)
+let getNum1 = function(x:number , ...rest:number[]){
+    for(let i = 0 ; i < rest.length ; i++){
+        console.log(rest[i]);
+        
+    }
+}
+
+getNum1(12,22,33,44,55,66);
