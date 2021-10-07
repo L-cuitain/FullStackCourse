43e40e555db7a3db07010479f95ece7128ccb821
L-cuitain <948527099@qq.com>
2021-10-07 14:29:08 +0800

    20211006

--- day03-过渡动画&插件/nuxt_plugin/pages/nav.vue
+++ day03-过渡动画&插件/nuxt_plugin/pages/nav.vue
@@ -7,6 +7,7 @@
       <van-grid-item icon="photo-o" text="文字" />
       <van-grid-item icon="photo-o" text="文字" />
     </van-grid>
+    <van-search value="123" placeholder="请输入搜索关键词" />
   </div>
 </template>
 
--- day03-过渡动画&插件/nuxt_plugin/plugins/vant.js
+++ day03-过渡动画&插件/nuxt_plugin/plugins/vant.js
@@ -2,10 +2,9 @@
 import Vue from 'vue';
 
 //引入vant组件
-import { Button , Grid , GridItem } from 'vant';
+import Vant from 'vant';
 import 'vant/lib/index.css';
 
-Vue.use(Button);
-Vue.use(Grid);
-Vue.use(GridItem);
+Vue.use(Vant);
+
 
